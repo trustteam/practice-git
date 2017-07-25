8fd3662a9e6af2e98c3cb464c4e7c15bba01dad8
le.mao <le.mao@mulodo.com>
2017-07-25 16:02:23 +0700

    Branch 2-2 update configuration

--- Config.php
+++ Config.php
@@ -1,3 +1,4 @@
 <?php
-$config['data'] = array('1' => 'This is Item 1');
-$config['test'] = array('1' => 'This is Item 2');
+$config['data']  = array('1' => 'This is Item 1');
+$config['title'] = 'This is title of the screen'; 
+$config['test']  = array('1' => 'This is Item 2');
