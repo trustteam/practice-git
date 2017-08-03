ab98d32b35fc50059c69bf4265791b3a031ce38f
le.mao <le.mao@mulodo.com>
2017-08-03 11:51:21 +0700

    Testing checkout commmit - test revert commit-1

--- Config.php
+++ Config.php
@@ -3,4 +3,5 @@ $config['data'] = array('1' => 'This is Item 1');
 $config['test'] = array('1' => 'This is Item 2');
 $config['conflict3'] = 'Test3'
 $config['conflict4'] = 'Test4'
-$config['hash-1']    = 'Hash 1'
+$config['hash-1'] = 'Hash 1'
+$cofig['has-2']
