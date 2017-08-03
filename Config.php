d039d38a02fc20e3333f5ad33f524f73f358948d
le.mao <le.mao@mulodo.com>
2017-08-03 11:22:07 +0700

    Testing checkout commmit -2

--- Config.php
+++ Config.php
@@ -4,3 +4,4 @@ $config['test'] = array('1' => 'This is Item 2');
 $config['conflict3'] = 'Test3'
 $config['conflict4'] = 'Test4'
 $congig['checkout-comit'] = 'test';
+$config['checkut-commit'] = 'temp';
